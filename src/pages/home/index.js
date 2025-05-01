609816e1aec1fc421917e70756ae80f97bedd4b3
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-05-01 21:06:42 +0900

    ui: 홈페이지, test페이지 구성

---
+++ src/pages/home/index.js
@@ -0,0 +1,10 @@
+import Menubar from "../../components/common/menubar";
+
+function HomePage() {
+  return (
+    <>
+      <h1>홈페이지</h1>
+    </>
+  );
+}
+export default HomePage;
---
+++ src/pages/test/index.js
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+function TestPage() {
+  return (
+    <>
+      <Wrapper>
+        <ItemBox>
+          <h1>2025 키오스크 만들기 스터디</h1>
+          <Link to={"/home"}>
+            <MyButton>시작하기</MyButton>
+          </Link>
+        </ItemBox>
+      </Wrapper>
+    </>
+  );
+}
+export default TestPage;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const ItemBox = styled.div`
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const MyButton = styled.button`
+  background-color: pink;
+  color: white;
+  padding: 10px 40px;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: bold;
+`;
