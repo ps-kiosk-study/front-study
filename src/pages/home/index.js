597c930900f75a45cc7bf0a58c6da54a28d07714
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-05-02 11:31:29 +0900

    feat: 홈화면 구성

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,20 @@
-import Menubar from "../../components/common/menubar";
+import ProductCard from "../../components/homepage/productCard";
+import styled from "styled-components";
 
 function HomePage() {
   return (
-    <>
-      <h1>홈페이지</h1>
-    </>
+    <Wrapper>
+      <ProductCard />
+    </Wrapper>
   );
 }
 export default HomePage;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center; /* 중앙 정렬 */
+  gap: 20px; /* 카드 간 간격 */
+  max-width: 600px; /* 한 줄에 3개 정도 들어가게 제한 */
+  margin: 0 auto; /* 가운데 정렬 */
+`;
